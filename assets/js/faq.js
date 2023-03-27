cd566b29b54af67facb1d6365776d1c63701cd01
akipfer <akipfer@users.noreply.github.com>
2023-03-27 14:09:32 -0300

    UpsYdates

---
+++ assets/js/faq.js
@@ -0,0 +1,26 @@
+// inicio js FAQ's
+var faq = document.getElementsByClassName("faq-page");
+	var i;
+
+	for (i = 0; i < faq.length; i++) {
+
+
+        if (i => 1){console.log("maior que um")} 
+        if (i => 1){console.log("total de i" . i)} 
+
+
+		faq[i].addEventListener("click", function () {
+			/* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+			this.classList.toggle("active");
+
+			/* Toggle between hiding and showing the active panel */
+			var body = this.nextElementSibling;
+			if (body.style.display === "block") {
+				body.style.display = "none";
+			} else {
+				body.style.display = "block";
+			}
+		});
+	}
+// FIM js FAQ's
